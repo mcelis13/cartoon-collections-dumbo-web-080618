6ea29dda54eb40d0d9ec2f8badbdd1b7f507d59b
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 15:57:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  newArray = array.collect { |index| puts "#{index}!"}
+  newArray = array.collect { |index| "#{index}!"}
   return newArray
 end
 
