7af0b7e77885e16f4c4e87f36966c06334c6894b
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 15:46:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(array)
   newArray = array.collections { |index| puts "#{index}.capitalize!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |index| index.size > 4}
 end
 
 def find_the_cheese# code an argument here
