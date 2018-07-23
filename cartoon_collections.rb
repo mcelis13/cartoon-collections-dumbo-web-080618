69f739e9b6c0436d3366c83d5e3195aa09d422ac
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 16:05:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,12 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
   for index in array do
     if cheese_types.include?(index)
       return true
     end
   end
+
   return nil
 end
