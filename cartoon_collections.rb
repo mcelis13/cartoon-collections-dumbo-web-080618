e3fd2f54214d5515a101827e43dbcdb17a95d004
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 15:43:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  newArray = array.collections { |index| puts "#{array.size}*#{index}"}
+  newArray = array.collections { |index| puts "#{array.size}*#{index} \n"}
   return newArray
 end
 
