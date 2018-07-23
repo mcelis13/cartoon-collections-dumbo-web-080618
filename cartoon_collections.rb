dbec13ace7907fbb49a89519ac789b3a73868a3a
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 15:55:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  newArray = array.collect { |index| puts "#{index}.capitalize!"}
+  newArray = array.collect { |index| puts "#{index.capitalize}!"}
 end
 
 def long_planeteer_calls(array)
